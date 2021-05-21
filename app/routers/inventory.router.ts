5f70739bf7254d731086f3a17a0c2684dcf483c6
herirudini <75158630+herirudini@users.noreply.github.com>
2021-05-21 21:31:09 +0700

    Update inventory.router.ts

--- app/routers/inventory.router.ts
+++ app/routers/inventory.router.ts
@@ -32,7 +32,7 @@ class inventoryRouter {
         this.router.patch('/product/:product_id', inventoryController.setProductStatus);
     }
     private confirmDelivery(): void {
-        this.router.post('/delivery', inventoryController.confirmDelivery);
+        this.router.post('/delivery-order', inventoryController.confirmDelivery);
     }
 }
 
