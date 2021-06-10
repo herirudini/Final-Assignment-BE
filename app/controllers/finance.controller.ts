e7e852e6ab8da238bf7a43814f57e6bba71ab822
herirudini <75158630+herirudini@users.noreply.github.com>
2021-06-10 15:33:23 +0700

    Update finance.controller.ts

--- app/controllers/finance.controller.ts
+++ app/controllers/finance.controller.ts
@@ -64,7 +64,7 @@ class financeController {
             getInvoices = await Invoice.find({ status: "paid", updatedAt: dateRange })
         }
         catch (err) {
-            next(err)
+            res.status(500).json({success: false, message: "gagal maning", err})
         }
         finally {
             res.status(200).json({ success: true, message: "paid invoices:", data: getInvoices })
