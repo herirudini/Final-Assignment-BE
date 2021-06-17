d4583ca83c701df1918db375942ad21569ed10fe
herirudini <75158630+herirudini@users.noreply.github.com>
2021-06-17 14:57:13 +0700

    debug

--- deploy/app/app.js
+++ deploy/app/app.js
@@ -10,7 +10,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const express_form_data_1 = __importDefault(require("express-form-data"));
 const os_1 = __importDefault(require("os"));
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser')
 class App {
     constructor() {
         dotenv_1.default.config();
@@ -21,14 +21,14 @@ class App {
         this.routes();
     }
     plugin() {
+        // this.app.use(bodyparser.json({ limit: '16mb' }));
+        // this.app.use(bodyparser.urlencoded({ extended: false }));
         this.app.use(express_1.default.json({ limit: '16mb' }));
         this.app.use(express_1.default.urlencoded({ extended: false }));
         this.app.use(express_form_data_1.default.parse({ uploadDir: os_1.default.tmpdir(), autoClean: true }));
         this.app.use(express_form_data_1.default.format());
         this.app.use(express_form_data_1.default.stream());
         this.app.use(express_form_data_1.default.union());
-        // this.app.use(bodyParser.json());
-        // this.app.use(bodyParser.urlencoded({ extended: false }));
     }
     cors() {
         this.app.use((req, res, next) => {
